31faec5174d658a1aed22cf2821a1c5c65e5b39e
Yimura <24669514+Yimura@users.noreply.github.com>
2025-04-12 18:12:23 +0200

    chore: Move GattSvrRegisterCb to Gatt.cpp from Ble.cpp

--- main/abstractions/ble/Gatt.cpp
+++ main/abstractions/ble/Gatt.cpp
@@ -26,6 +26,37 @@ namespace RemoteUnlock
         return true;
     }
 
+    void Ble::GattSvrRegisterCb(ble_gatt_register_ctxt* ctxt, void* arg)
+    {
+        char bleUUID[BLE_UUID_STR_LEN];
+
+        switch (ctxt->op)
+        {
+        case BLE_GATT_REGISTER_OP_SVC:
+            std::cout << "Registered bluetooth service " << ble_uuid_to_str(ctxt->svc.svc_def->uuid, bleUUID)
+                      << " with handle=" << ctxt->svc.handle << std::endl;
+
+            break;
+
+        case BLE_GATT_REGISTER_OP_CHR:
+            std::cout << "Registered bluetooth characteristic " << ble_uuid_to_str(ctxt->chr.chr_def->uuid, bleUUID)
+                      << " with handle=" << ctxt->chr.def_handle << std::endl;
+
+            break;
+
+        case BLE_GATT_REGISTER_OP_DSC:
+            std::cout << "Registered bluetooth descriptor " << ble_uuid_to_str(ctxt->dsc.dsc_def->uuid, bleUUID)
+                      << " with handle=" << ctxt->dsc.handle << std::endl;
+
+            break;
+
+        default:
+            std::cout << "Unknown event caught: " << ctxt->op << std::endl;
+
+            break;
+        }
+    }
+
     void Ble::GattSvrSubscribe(ble_gap_event* event)
     {
     }
